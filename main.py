4b85569d6d42d61d9ba926103b0f83a1d4a280d4
Ben Harper <rogojin@gmail.com>
2024-11-03 15:33:38 +0200

    Fix sampling regression

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from machine import ADC, Pin
 import sys
 
 # Wi-Fi Credentials
-ssid = 'YOUR SSID'
-password = 'YOUR PASSWORD'
+ssid = 'SSID'
+password = 'PASSWORD'
 server_addr = '192.168.68.110'
 server_port = '8001'
 
@@ -59,13 +59,15 @@ def send_pressure(adc_value):
         response.close()
     except Exception as e:
         print(f'Error making HTTP request: {e}')
-        blink(2, 300)
+        blink(2, 500)
 
 # Connect to Wi-Fi initially
 connect_wifi()
 
 # Send a bogus value so that we know the system is awake
-send_pressure(12345)
+#send_pressure(12345)
+# Let's rather send an unfiltered, but real value
+send_pressure(adc.read_u16())
 
 while True:
     # Ensure Wi-Fi connection is active
@@ -74,11 +76,13 @@ while True:
         connect_wifi()
 
     # Take 60 measurements 1 second apart, and send the median value
-    measures = [adc.read_u16() for _ in range(60)]
-    utime.sleep(1)  # Delay between each measurement
+    measures = []
+    for i in range(60):
+        measures.append(adc.read_u16())
+        utime.sleep(1)  # Delay between each measurement
 
     median = sorted(measures)[len(measures) // 2]
     send_pressure(median)
 
-    blink(6, 30)  # Indicate data sent
-    utime.sleep(60)  # Delay before the next reading
+    blink(3, 50)  # Indicate data sent
+    #utime.sleep(60)  # Delay before the next reading
